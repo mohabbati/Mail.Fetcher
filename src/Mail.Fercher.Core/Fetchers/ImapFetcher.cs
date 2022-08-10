0d6dc14589210dfc467aef2019c18a2307acfa5b
Mohammad Mohabbati <mohabbati@live.com>
2022-08-10 13:36:19 +0300

    Refactored ImapFetcher

--- src/Mail.Fercher.Core/Fetchers/ImapFetcher.cs
+++ src/Mail.Fercher.Core/Fetchers/ImapFetcher.cs
@@ -6,26 +6,67 @@ namespace Mail.Fercher.Core;
 
 public class ImapFetcher : IFetcher
 {
-    private const int maxDegreeOfParallelism = 2;
+    private readonly FetchRequest _fetchRequest;
 
-    public async Task<List<MailMessage>> FetchAsync(MailServerConnection mailServerConnection, FetchRequest fetchRequest, CancellationToken cancellationToken)
+    public ImapFetcher()
+    {
+        _fetchRequest = new FetchRequest { Query = MailKit.Search.SearchQuery.All };
+    }
+    
+    public ImapFetcher(FetchRequest fetchRequest)
+    {
+        _fetchRequest = fetchRequest;
+    }
+
+    public async Task<List<MailMessage>> FetchAsync(MailServerConnection mailServerConnection, CancellationToken cancellationToken)
     {
         using var client = new ImapClient();
 
-        await client.ConnectAsync(mailServerConnection.Host, mailServerConnection.Port, SecureSocketOptions.Auto, cancellationToken);
+        await ConnectTo(client, mailServerConnection, cancellationToken);
 
-        await client.AuthenticateAsync(mailServerConnection.UserName, mailServerConnection.Password, cancellationToken);
+        return await FetchAsync(client, cancellationToken);
+    }
+
+    private async Task<List<MailMessage>> FetchAsync(ImapClient client, CancellationToken cancellationToken)
+    {
+        await client.Inbox.OpenAsync(FolderAccess.ReadOnly, cancellationToken);
+
+        var uniqueIds = await client.Inbox.SearchAsync(_fetchRequest.Query, cancellationToken);
+
+        var mailMessages = new List<MailMessage>();
+
+        foreach (var item in uniqueIds)
+        {
+            var message = client.Inbox.GetMessage(item);
+            mailMessages.Add(new() { MimeMessage = message });
+        }
+
+        await client.DisconnectAsync(true, cancellationToken);
+
+        return mailMessages;
+    }
+
+    public async Task<List<MailMessage>> FetchParallelAsync(FetcherConfiguration fetcherConfiguration, MailServerConnection mailServerConnection, CancellationToken cancellationToken)
+    {
+        using var client = new ImapClient();
+
+        await ConnectTo(client, mailServerConnection, cancellationToken);
 
         await client.Inbox.OpenAsync(FolderAccess.ReadOnly, cancellationToken);
 
-        var uniqueIds = await client.Inbox.SearchAsync(fetchRequest.Query, cancellationToken);
+        var uniqueIds = await client.Inbox.SearchAsync(_fetchRequest.Query, cancellationToken);
+
+        if (uniqueIds.Count < fetcherConfiguration.MinimumMessageToEnableParallel)
+        {
+            return await FetchAsync(client, cancellationToken);
+        }
 
         var mailMessages = new List<MailMessage>();
 
         await Parallel.ForEachAsync(uniqueIds, new ParallelOptions
         {
             CancellationToken = cancellationToken,
-            MaxDegreeOfParallelism = maxDegreeOfParallelism
+            MaxDegreeOfParallelism = fetcherConfiguration.MaxDegreeOfParallelism
         },
             async (uniqueId, taskCancellationToken) =>
             {
@@ -36,5 +77,12 @@ public class ImapFetcher : IFetcher
         await client.DisconnectAsync(true, cancellationToken);
 
         return mailMessages;
+    }
+
+    private async Task ConnectTo(ImapClient client, MailServerConnection mailServerConnection, CancellationToken cancellationToken)
+    {
+        await client.ConnectAsync(mailServerConnection.Host, mailServerConnection.Port, SecureSocketOptions.Auto, cancellationToken);
+
+        await client.AuthenticateAsync(mailServerConnection.UserName, mailServerConnection.Password, cancellationToken);
     }
 }
