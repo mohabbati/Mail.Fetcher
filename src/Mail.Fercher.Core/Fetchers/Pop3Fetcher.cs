b4009759deac76f18385039233b27905cdfa6daf
Mohammad Mohabbati <mohabbati@live.com>
2022-08-10 10:24:41 +0300

    Refactor IFetcher and pop3 implementation

--- src/Mail.Fercher.Core/Fetchers/Pop3Fetcher.cs
+++ src/Mail.Fercher.Core/Fetchers/Pop3Fetcher.cs
@@ -5,34 +5,74 @@ namespace Mail.Fercher.Core;
 
 public class Pop3Fetcher : IFetcher
 {
-    private static int maxDegreeOfParallelism = 3;
-
-    public async Task<List<MailMessage>> FetchAsync(MailServerConnection mailServerConnection, FetchRequest fetchRequest, CancellationToken cancellationToken)
+    public async Task<List<MailMessage>> FetchAsync(MailServerConnection mailServerConnection, CancellationToken cancellationToken)
     {
         using var client = new Pop3Client();
 
-        await client.ConnectAsync(mailServerConnection.Host, mailServerConnection.Port, SecureSocketOptions.Auto);
+        await ConnectTo(client, mailServerConnection, cancellationToken);
 
-        await client.AuthenticateAsync(mailServerConnection.UserName, mailServerConnection.Password);
+        return await FetchAsync(client, cancellationToken);
+    }
 
+    private async Task<List<MailMessage>> FetchAsync(Pop3Client client, CancellationToken cancellationToken)
+    {
         var mailMessages = new List<MailMessage>();
 
-        var indexes = Enumerable.Range(0, mailMessages.Count - 1).ToList();
+        for (int i = 0; i < client.Count; i++)
+        {
+            var message = await GetMessageAsync(client, i, cancellationToken);
+            mailMessages.Add(message);
+        }
+
+        await client.DisconnectAsync(true, cancellationToken);
+
+        return mailMessages;
+    }
+
+    public async Task<List<MailMessage>> FetchParallelAsync(FetcherConfiguration fetcherConfiguration, MailServerConnection mailServerConnection, CancellationToken cancellationToken)
+    {
+        using var client = new Pop3Client();
+
+        await ConnectTo(client, mailServerConnection, cancellationToken);
+
+        if (client.Count < fetcherConfiguration.MinimumMessageToEnableParallel)
+        {
+            return await FetchAsync(client, cancellationToken);
+        }
+
+        var indexes = Enumerable.Range(0, client.Count - 1).ToList().AsReadOnly();
+
+        var mailMessages = new List<MailMessage>();
 
         await Parallel.ForEachAsync(indexes, new ParallelOptions
         {
             CancellationToken = cancellationToken,
-            MaxDegreeOfParallelism = maxDegreeOfParallelism
+            MaxDegreeOfParallelism = fetcherConfiguration.MaxDegreeOfParallelism
         },
             async (index, taskCancellationToken) =>
             {
-                var message = await client.GetMessageAsync(index, taskCancellationToken);
-                mailMessages.Add(new() { MimeMessage = message });
-                await client.DeleteMessageAsync(index, cancellationToken);
+                var message = await GetMessageAsync(client, index, taskCancellationToken);
+                mailMessages.Add(message);
             });
 
         await client.DisconnectAsync(true, cancellationToken);
 
         return mailMessages;
+    }
+
+    private async Task ConnectTo(Pop3Client client, MailServerConnection mailServerConnection, CancellationToken cancellationToken)
+    {
+        await client.ConnectAsync(mailServerConnection.Host, mailServerConnection.Port, SecureSocketOptions.Auto);
+
+        await client.AuthenticateAsync(mailServerConnection.UserName, mailServerConnection.Password);
+    }
+
+    private async Task<MailMessage> GetMessageAsync(Pop3Client client, int index, CancellationToken cancellationToken)
+    {
+        var message = await client.GetMessageAsync(index, cancellationToken);
+
+        await client.DeleteMessageAsync(index, cancellationToken);
+
+        return new() { MimeMessage = message };
     }
 }
