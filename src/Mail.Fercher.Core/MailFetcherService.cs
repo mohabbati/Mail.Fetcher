43bff484ee6bbddd519872d091e4b3ee84d5f1da
mohabbati <mohabbati@live.com>
2022-08-21 18:40:30 +0300

    Add MailFetcherService (#16)
    * Added mail fetcher service
    
    * Refactor MailFetcherService

---
+++ src/Mail.Fercher.Core/MailFetcherService.cs
@@ -0,0 +1,116 @@
+﻿using Mail.Fercher.Core.Models;
+using System.Collections.Concurrent;
+
+namespace Mail.Fercher.Core;
+
+public class MailFetcherService
+{
+    private readonly IServiceProvider _serviceProvider;
+    private readonly LinkedList<MailFetcherRequest> _mailFetcherRequests; //TODO: use builder to create new object
+
+    public MailFetcherService(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+
+        _mailFetcherRequests = new();
+    }
+
+    /// <summary>
+    /// CLear all fetchers.
+    /// </summary>
+    public void ClearFetchers()
+    {
+        _mailFetcherRequests.Clear();
+    }
+
+    /// <summary>
+    /// Configure fetcher to fetch messages from the mail server
+    /// </summary>
+    /// <param name="mailFetcherRequest"></param>
+    public void ConfigureFetchers(MailFetcherRequest mailFetcherRequest)
+    {
+        ArgumentNullException.ThrowIfNull(mailFetcherRequest);
+
+        _mailFetcherRequests.AddLast(mailFetcherRequest);
+    }
+
+    /// <summary>
+    /// Configure fetchers to fetch messages from the mail servers
+    /// </summary>
+    /// <param name="mailFetcherRequest"></param>
+    public void ConfigureFetchers(List<MailFetcherRequest> mailFetcherRequests)
+    {
+        ArgumentNullException.ThrowIfNull(mailFetcherRequests);
+
+        mailFetcherRequests.ForEach(x =>
+        {
+            _mailFetcherRequests.AddLast(x);
+        });
+    }
+
+    /// <summary>
+    /// Fetch mail messages from the server based on all the configation.
+    /// </summary>
+    /// <param name="mailFetcherRequest"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public async Task<List<MailMessage>> InvokeAsync(CancellationToken cancellationToken)
+    {
+        return await InvokeAsync(new ParallelOptions
+        {
+            CancellationToken = cancellationToken,
+            MaxDegreeOfParallelism = 1
+        }, cancellationToken);
+    }
+
+    /// <summary>
+    /// Fetch mail messages from the servers list based on all the configation.
+    /// </summary>
+    /// <param name="parallelOptions"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public async Task<List<MailMessage>> InvokeAsync(ParallelOptions parallelOptions, CancellationToken cancellationToken)
+    {
+        var allMailMessages = new ConcurrentBag<MailMessage>();
+
+        var imapFetcher = (MailFetcher<IFetcher>?)_serviceProvider.GetService(typeof(MailFetcher<ImapFetcher>));
+        var pop3Fetcher = (MailFetcher<IFetcher>?)_serviceProvider.GetService(typeof(MailFetcher<Pop3Fetcher>));
+
+        ArgumentNullException.ThrowIfNull(imapFetcher);
+        ArgumentNullException.ThrowIfNull(pop3Fetcher);
+
+        await Parallel.ForEachAsync(_mailFetcherRequests, parallelOptions, async (item, taskCancellationToken) =>
+        {
+            List<MailMessage> fetcherMailMessages;
+            if (item.ProtocolType == ProtocolType.Imap)
+            {
+                fetcherMailMessages = await GetFetcherMailMessages(item, imapFetcher, cancellationToken);
+            }
+            else if (item.ProtocolType == ProtocolType.Pop3)
+            {
+                fetcherMailMessages = await GetFetcherMailMessages(item, pop3Fetcher, cancellationToken);
+            }
+            else
+            {
+                fetcherMailMessages = new();
+            }
+            allMailMessages.Concat(fetcherMailMessages);
+        });
+
+        return allMailMessages.ToList();
+    }
+
+    private async Task<List<MailMessage>> GetFetcherMailMessages(MailFetcherRequest mailFetcherRequest, MailFetcher<IFetcher> imapFetcher, CancellationToken cancellationToken)
+    {
+        imapFetcher.ConfigureFetcher(mailFetcherRequest.FetcherConfiguration);
+        imapFetcher.ConfigureConnection(mailFetcherRequest.MailServerConnection);
+        if (mailFetcherRequest.FetchRequest is not null)
+        {
+            imapFetcher.ConfigureFetchRequest(mailFetcherRequest.FetchRequest);
+        }
+
+        var messages = await imapFetcher.InvokeAsync(cancellationToken);
+
+        return messages;
+    }
+}
---
+++ src/Mail.Fercher.Core/Models/MailFetcherRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace Mail.Fercher.Core.Models;
+
+public class MailFetcherRequest
+{
+    public FetcherConfiguration FetcherConfiguration { get; set; }
+
+    public MailServerConnection MailServerConnection { get; set; }
+
+    public FetchRequest? FetchRequest { get; set; }
+
+    public ProtocolType ProtocolType { get; set; }
+}
+
+public enum ProtocolType
+{
+    Imap,
+    Pop3
+}
